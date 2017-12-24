b14a5655c2a34f95bd8ec9aed0cb1124c95a797c
Sharkeee <sharkmen1996@gmail.com>
2017-12-24 13:06:41 +0100

    Fundamenty + deposit

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,54 @@
 public class Account {
 
-    
+    private String number;
+    private double balance;
+    private String customerName;
+    private String customerMail;
+    private String customerCellphone;
+
+
+    public Account() {
+        this("00000", 2.5, "Przemek Kijak", "kijak@gmail.com", "79023176");
+    }
+
+    public Account(String number, double balance, String customerName, String customerMail, String customerCellphone) {
+        this.number = number;
+        this.balance = balance;
+        this.customerName = customerName;
+        this.customerMail = customerMail;
+        this.customerCellphone = customerCellphone;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerMail() {
+        return customerMail;
+    }
+
+    public String getCustomerCellphone() {
+        return customerCellphone;
+    }
+
+    public void deposit(double depositValue) {
+        this.balance += depositValue;
+        System.out.println("Desposit successfull - " + depositValue);
+        System.out.println("[" + this.number + "] Your account balance is: " + this.balance);
+
+    }
 }
+
+
+
+
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Account firstAccount = new Account();
+
+        System.out.println("Account information: \n" +
+                firstAccount.getCustomerName() + "\n" +
+                firstAccount.getCustomerMail() + "\n" +
+                firstAccount.getCustomerCellphone() + "\n" +
+                firstAccount.getNumber() + "\n" +
+                firstAccount.getBalance() + "\n");
+
+                firstAccount.deposit(5.0);
+    }
+
 }
